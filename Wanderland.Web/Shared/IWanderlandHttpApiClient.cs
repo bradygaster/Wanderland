31370a4cfeeb62bb608f2e207351f954f9a2dc7e
bradygaster <bradyg@microsoft.com>
2022-02-25 16:14:35 -0800

    worlds showing up in UI

---
+++ Wanderland.Web/Shared/IWanderlandHttpApiClient.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+
+namespace Wanderland.Web.Shared
+{
+    public interface IWanderlandHttpApiClient
+    {
+        [Get("/worlds")]
+        Task<List<World>> GetWorlds();
+    }
+
+    public class WanderlandHttpApiClient : IWanderlandHttpApiClient
+    {
+        HttpClient _httpClient;
+
+        public WanderlandHttpApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<World>> GetWorlds()
+        {
+            return await RestService.For<IWanderlandHttpApiClient>(_httpClient).GetWorlds();
+        }
+    }
+}
